3d4f61c4334f35d09d153fdbdb883cac73c43623
xinle1030 <xinlelam1030@gmail.com>
2023-08-05 20:47:00 +0800

    complete payment routes

---
+++ app/controllers/payment/index.js
@@ -0,0 +1,11 @@
+const createPayment = require("./createPayment");
+const updatePayment = require("./updatePayment");
+const viewPayment = require("./viewPayment");
+const deletePayment = require("./deletePayment");
+
+module.exports = {
+  createPayment,
+  updatePayment,
+  viewPayment,
+  deletePayment,
+};
---
+++ app/routes/payment.routes.js
@@ -0,0 +1,31 @@
+const {
+  createPayment,
+  updatePayment,
+  viewPayment,
+  deletePayment,
+} = require("../controllers/payment");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/payments",
+    createPayment.createPayment
+  );
+
+  app.get("/api/payments/all", viewPayment.getAllPayments);
+
+  app.get("/api/payments/:id", viewPayment.getPaymentById);
+
+  app.put("/api/payments/:id", updatePayment.updatePaymentById);
+
+  app.delete("/api/payments/all", deletePayment.deleteAllPayments);
+
+  app.delete("/api/payments/:id", deletePayment.deletePaymentById);
+};
