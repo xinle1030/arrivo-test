9a109c3322a7b16454372d0e55a7f310c5e1fcab
xinle1030 <xinlelam1030@gmail.com>
2023-08-05 18:14:01 +0800

    complete category routes

---
+++ app/controllers/category/index.js
@@ -0,0 +1,11 @@
+const createCategory = require("./createCategory");
+const updateCategory = require("./updateCategory");
+const viewCategory = require("./viewCategory");
+const deleteCategory = require("./deleteCategory");
+
+module.exports = {
+  createCategory,
+  updateCategory,
+  viewCategory,
+  deleteCategory,
+};
---
+++ app/routes/category.routes.js
@@ -0,0 +1,31 @@
+const {
+  createCategory,
+  updateCategory,
+  viewCategory,
+  deleteCategory,
+} = require("../controllers/category");
+
+module.exports = function (app) {
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.post(
+    "/api/categories",
+    createCategory.createCategory
+  );
+
+  app.get("/api/categories/all", viewCategory.getAllCategories);
+
+  app.get("/api/categories/:id", viewCategory.getCategoryById);
+
+  app.put("/api/categories/:id", updateCategory.updateCategoryById);
+
+  app.delete("/api/categories/all", deleteCategory.deleteAllCategories);
+
+  app.delete("/api/categories/:id", deleteCategory.deleteCategoryById);
+};
